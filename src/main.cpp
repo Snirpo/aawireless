85f9410482fcc9ab3d26e536802b765106a77843
Chiel Prins <snirpo@gmail.com>
2019-12-25 13:45:05 +0100

    initial setup of aasdk

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,84 @@
-#include <iostream>
 #include <QCoreApplication>
+#include <thread>
+#include <libusb.h>
 #include "aawireless/bluetooth/BluetoothService.h"
+#include <boost/asio/io_service.hpp>
+#include <aawireless/log/Log.h>
+#include <f1x/aasdk/TCP/TCPWrapper.hpp>
+#include <f1x/aasdk/USB/USBWrapper.hpp>
+#include <f1x/aasdk/USB/USBHub.hpp>
+#include <f1x/aasdk/USB/AccessoryModeQueryFactory.hpp>
+#include <f1x/aasdk/USB/AccessoryModeQueryChainFactory.hpp>
+#include <f1x/aasdk/USB/ConnectedAccessoriesEnumerator.hpp>
+
+using ThreadPool = std::vector<std::thread>;
+
+void startUSBWorkers(boost::asio::io_service& ioService, libusb_context* usbContext, ThreadPool& threadPool)
+{
+    auto usbWorker = [&ioService, usbContext]() {
+        timeval libusbEventTimeout{180, 0};
+
+        while(!ioService.stopped())
+        {
+            libusb_handle_events_timeout_completed(usbContext, &libusbEventTimeout, nullptr);
+        }
+    };
+
+    threadPool.emplace_back(usbWorker);
+    threadPool.emplace_back(usbWorker);
+    threadPool.emplace_back(usbWorker);
+    threadPool.emplace_back(usbWorker);
+}
+
+void startIOServiceWorkers(boost::asio::io_service& ioService, ThreadPool& threadPool)
+{
+    auto ioServiceWorker = [&ioService]() {
+        ioService.run();
+    };
+
+    threadPool.emplace_back(ioServiceWorker);
+    threadPool.emplace_back(ioServiceWorker);
+    threadPool.emplace_back(ioServiceWorker);
+    threadPool.emplace_back(ioServiceWorker);
+}
 
 int main(int argc, char *argv[]) {
+    libusb_context* usbContext;
+    if(libusb_init(&usbContext) != 0)
+    {
+        AW_LOG(error) << "[OpenAuto] libusb init failed.";
+        return 1;
+    }
+
+    boost::asio::io_service ioService;
+    boost::asio::io_service::work work(ioService);
+    std::vector<std::thread> threadPool;
+    startUSBWorkers(ioService, usbContext, threadPool);
+    startIOServiceWorkers(ioService, threadPool);
+
     QCoreApplication qApplication(argc, argv);
+
+    f1x::aasdk::tcp::TCPWrapper tcpWrapper;
+    f1x::aasdk::usb::USBWrapper usbWrapper(usbContext);
+    f1x::aasdk::usb::AccessoryModeQueryFactory queryFactory(usbWrapper, ioService);
+    f1x::aasdk::usb::AccessoryModeQueryChainFactory queryChainFactory(usbWrapper, ioService, queryFactory);
+    //autoapp::service::ServiceFactory serviceFactory(ioService, configuration);
+    //autoapp::service::AndroidAutoEntityFactory androidAutoEntityFactory(ioService, configuration, serviceFactory);
+
+    auto usbHub(std::make_shared<f1x::aasdk::usb::USBHub>(usbWrapper, ioService, queryChainFactory));
+    auto connectedAccessoriesEnumerator(std::make_shared<f1x::aasdk::usb::ConnectedAccessoriesEnumerator>(usbWrapper, ioService, queryChainFactory));
+    //auto app = std::make_shared<autoapp::App>(ioService, usbWrapper, tcpWrapper, androidAutoEntityFactory, std::move(usbHub), std::move(connectedAccessoriesEnumerator));
 
     auto bluetoothService = std::make_shared<aawireless::bluetooth::BluetoothService>();
     bluetoothService->start();
 
-    qApplication.exec();
+    auto result = qApplication.exec();
+
+    std::for_each(threadPool.begin(), threadPool.end(), std::bind(&std::thread::join, std::placeholders::_1));
 
     bluetoothService->stop();
 
-    return 0;
+    libusb_exit(usbContext);
+
+    return result;
 }
